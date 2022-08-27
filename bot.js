fca8cf9e980242a5eb75c4f6e226aa8dc21b96b6
Glitch (glitch-hello-website) <none>
2022-08-27 15:07:54 +0000

    👜🏯 Checkpoint ./bot.js:26026384/297

--- bot.js
+++ bot.js
@@ -4,20 +4,13 @@ const bot = new Client();
 
 bot.on("ready", async() => {
     console.log(`[ Client ] ${bot.user.tag} esta online agora!`);
- setTimeout(  
+
     bot.user.setPresence({
         status: 'idle',
         activity: {
-            name: "PINGOBRAS HOST.glitch.me",
-            type: "WATCHING",}}),10000),
-  setTimeout( 
-        bot.user.setPresence({
-        status: 'dnd',
-        activity: {
-            name: "SERVER HOST.glitch.me",
-            type: "LISTENING",}}),15000)
-  
-  
+            name: "pingobras.glitch.me" + new Date().getFullYear(),
+            type: "WATCHING",}})
+       
 })
 
 
